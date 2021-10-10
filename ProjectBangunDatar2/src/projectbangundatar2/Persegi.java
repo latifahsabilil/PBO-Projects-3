cbc59ea21c4efe5ffda85f6fa71be6e54a5a497f
latifahsabilil <71626855+latifahsabilil@users.noreply.github.com>
2021-10-10 08:36:05 +0700

    Create Persegi.java

---
+++ ProjectBangunDatar2/src/projectbangundatar2/Persegi.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Persegi {
+    void luasPersegi(double sisi){
+        double luas = sisi * sisi;
+        System.out.println("Luas persegi tersebut adalah " + luas + " satuan luas");
+    }
+    
+    void luasPersegi(int sisi){
+        int luas = sisi * sisi;
+        System.out.println("Luas persegi tersebut adalah " + luas + " satuan luas");
+    }
+    
+    void kelilingPersegi(double sisi){
+        double keliling = 4 * sisi;
+        System.out.println("Keliling persegi tersebut adalah " + keliling + " satuan luas");
+    }
+        
+    void kelilingPersegi(int sisi){
+        int keliling = 4 * sisi;
+        System.out.println("Keliling persegi tersebut adalah " + keliling + " satuan luas");
+    }
+}
