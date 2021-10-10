56b6bb82aa7a607fb9702dbd54866ed45cf804d6
latifahsabilil <71626855+latifahsabilil@users.noreply.github.com>
2021-10-10 08:36:08 +0700

    Create PersegiPanjang.java

---
+++ ProjectBangunDatar2/src/projectbangundatar2/PersegiPanjang.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class PersegiPanjang {
+    void luasPPanjang(int a, int b){
+        int luas = a * b;
+        System.out.println("Luas persegi panjang tersebut adalah " + luas + " satuan luas");
+    }
+    
+    void luasPPanjang(double a, int b){
+        double luas = a * b;
+        System.out.println("Luas persegi panjang tersebut adalah " + luas + " satuan luas");
+    }
+    
+    void luasPPanjang(int a, double b){
+        double luas = a * b;
+        System.out.println("Luas persegi panjang tersebut adalah " + luas + " satuan luas");
+    }
+    
+    void luasPPanjang(double a, double b){
+        double luas = a * b;
+        System.out.println("Luas persegi panjang tersebut adalah " + luas + " satuan luas");
+    }
+    
+    void kelilingPPanjang(int a, int b){
+        int keliling = 2 * ( a + b );
+        System.out.println("Keliling persegi panjang tersebut adalah " + keliling + " satuan luas");
+    }
+    
+    void kelilingPPanjang(double a, int b){
+        double keliling = 2 * ( a + b );
+        System.out.println("Keliling persegi panjang tersebut adalah " + keliling + " satuan luas");
+    }
+    
+    void kelilingPPanjang(int a, double b){
+        double keliling = 2 * ( a + b );
+        System.out.println("Keliling persegi panjang tersebut adalah " + keliling + " satuan luas");
+    }
+    
+    void kelilingPPanjang(double a, double b){
+        double keliling = 2 * ( a + b );
+        System.out.println("Keliling persegi panjang tersebut adalah " + keliling + " satuan luas");
+    }
+}
